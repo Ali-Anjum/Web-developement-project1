ea2b3867c1e66b0464bb3d7aa7a114bd6fd0386e
Ali anjum <124556189+Ali-Anjum@users.noreply.github.com>
2023-03-12 18:38:56 +1400

    Update index.js

--- Day1started-completed 3days/Random color website/index.js
+++ Day1started-completed 3days/Random color website/index.js
@@ -1,56 +1,27 @@
 const refreshbtn = document.querySelector(".refresh-btn");
-maxplt=20
+function randomcol1 (){
+    randomcol =Math.floor(Math.random() * 0xffffff).toString(16);
+    randomcol=`${randomcol.padStart(6,"0")}`;
+    randomcol="#"+randomcol;
+    return randomcol
+}
 const gencolors= () => {
-    randomcol =Math.floor(Math.random() * 0xffffff).toString(16);
-    randomcol=`${randomcol.padStart(6,"0")}`;
-    document.getElementById("div3").style.backgroundColor ="#"+ randomcol;
-    document.getElementById("cdc3").innerHTML=`<p>${randomcol}</p>`
-    
-gencolors2()
-gencolors3()
-gencolors4()
+    let y = randomcol1()
+    document.body.style.backgroundColor =y;
+    document.getElementById("rt").innerHTML=` <p>${y}</p>`
+    document.getElementById("refresh-btn").style.color=y
 
-    
-}
-const gencolors2= () => {
-    console.log("hi")
-    randomcol =Math.floor(Math.random() * 0xffffff).toString(16);
-    console.log("h1i")
-    randomcol=`${randomcol.padStart(6,"0")}`;
-    console.log("hi2")
-    document.getElementById("div1").style.backgroundColor ="#"+ randomcol;
-    console.log("hi3")
-    document.getElementById("cdc").innerHTML=`<p>${randomcol}</p>`
-    console.log("hi4")
-    randomcol.addEventListener(click,event.clipboardData.setData={"hi"})
-    clipboardData.setData('text/plain', 'foo');
-    preventDefault();
-
-
-    
 }
 
-const gencolors3= () => {
-    randomcol =Math.floor(Math.random() * 0xffffff).toString(16);
-    randomcol=`${randomcol.padStart(6,"0")}`;
-    document.getElementById("div2").style.backgroundColor ="#"+ randomcol;
-    document.getElementById("cdc2").innerHTML=`<p>${randomcol}</p>`
-}
-const gencolors4= () => {
-    randomcol =Math.floor(Math.random() * 0xffffff).toString(16);
-    randomcol=`${randomcol.padStart(6,"0")}`;
-    document.getElementById("div4").style.backgroundColor ="#"+ randomcol;
-    document.getElementById("cdc4").innerHTML=`<p>${randomcol}</p>`
+const coppys=()=>{
+
+    navigator.clipboard.writeText(randomcol);
+    console.log(randomcol)
+alert("copied")
 }
 
-
-
-
-var randomcol=54
-console.log(randomcol)
 
 refreshbtn.addEventListener("click",gencolors );
-
-
-
 gencolors()
+
+document.getElementById("rt").addEventListener("click",coppys)
